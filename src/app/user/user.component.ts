a338a72b6a77295b9bc5dedbf4918ffa655eb1c8
Nishantpatil45 <nishantpatil4545@gmail.com>
2025-03-31 15:33:25 +0530

    Integrated separate componenet for adding new task and added local storage for adding and complete tasks

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,22 +1,24 @@
 import { Component, Output, EventEmitter, Input } from '@angular/core';
+import { type User } from './user.model';
+import { CardComponent } from "../shared/card/card.component";
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required:true}) id!: string;
-  @Input({required:true}) avatar!: string;
-  @Input({required:true}) name!: string;
+  
+  @Input({ required: true}) user!: User;
+  @Input({ required: true }) selected!: boolean;
   @Output() select = new EventEmitter();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
