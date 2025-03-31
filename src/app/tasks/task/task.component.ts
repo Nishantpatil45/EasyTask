efa5b91d59edb4b10a2f63737d144bd16d91cdfe
Nishantpatil45 <nishantpatil4545@gmail.com>
2025-03-31 15:08:07 +0530

    added separate component for task.

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,19 +1,19 @@
-import { Component, Input } from '@angular/core';
-
-interface Task {
-  id: string;
-  userId: string;
-  title: string;
-  summary: string;
-  dueDate: string;
-}
+import { Component, inject, Input } from '@angular/core';
+import { type Task } from './task.model';
+import { DatePipe } from '@angular/common';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-task',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
 export class TaskComponent {
   @Input({ required: true }) task!: Task;
+  private tasksService = inject(TasksService);
+
+  onCompleteTask() {
+    this.tasksService.removetask(this.task.id);
+  }
 }
