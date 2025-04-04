135ccfcc246efe3b0dbec2333376a6bd7eebb1e2
Nishantpatil45 <nishantpatil4545@gmail.com>
2025-04-04 11:14:07 +0530

    Converted whole application from Component base to Module Base Architecture

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,11 +1,10 @@
 import { Component, inject, Input } from '@angular/core';
 import { type Task } from './task.model';
-import { DatePipe } from '@angular/common';
 import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-task',
-  imports: [DatePipe],
+  standalone: false,
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
